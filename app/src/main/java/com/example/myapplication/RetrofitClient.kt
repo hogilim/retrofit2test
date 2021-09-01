36aecde66bfadd64ce1f5baf0966820276f77ba0
Lim Hogi <limhogi@Lims-MacBook-Air.local>
2021-09-02 00:19:18 +0900

    http post

--- app/src/main/java/com/example/myapplication/RetrofitClient.kt
+++ app/src/main/java/com/example/myapplication/RetrofitClient.kt
@@ -12,7 +12,7 @@ object RetrofitClient {
     fun getInstnace() : Retrofit {
         if(instance == null){
             instance = Retrofit.Builder()
-                .baseUrl("http://192.168.219.102:8080/test/")
+                .baseUrl("http://localhost:8080/test/")
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 //.addConverterFactory(ScalarsConverterFactory.create())
                 .build()
